7a0fb5b749dc96e59b915c2bb13e8f0c9f498103
Saurav Singh <singh.saurav@icloud.com>
2024-10-16 09:28:04 +0530

    added CodeWars

--- src/test/DS-Algo/DataStructure/LinkedList/LinkedList.java
+++ src/test/DS-Algo/DataStructure/LinkedList/LinkedList.java
@@ -80,4 +80,73 @@ public class LinkedList {
         }
         return temp;
     }
+
+    public Node get(int index) {
+        if (index < 0 || index > length) {
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        }
+
+        if (index == 0) {
+            prepend(value);
+        } else if (index == length) {
+            append(value);
+        } else {
+            Node newNode = new Node(value);
+            Node temp = get(index - 1);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            length++;
+        }
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == length - 1) {
+            return removeLast();
+        }
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        length--;
+        return temp;
+    }
+
+    public void reverse(){
+        Node temp=head;
+        head=tail;
+        tail=temp;
+        Node after=temp.next;
+        Node before=null;
+        for(int i=0;i<length;i++){
+            after=temp.next;
+            temp.next=before;
+            before=temp;
+            temp=after;
+        }
+    }
 }
