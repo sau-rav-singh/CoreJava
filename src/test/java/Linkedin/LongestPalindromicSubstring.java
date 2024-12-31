75389a35815a8219d9c4af99e75a4bcf0a90c8eb
Saurav Singh <singh.saurav@icloud.com>
2024-12-31 12:18:32 +0530

    added Linkedin Code

--- src/test/java/Linkedin/LongestPalindromicSubstring.java
+++ src/test/java/Linkedin/LongestPalindromicSubstring.java
@@ -2,39 +2,36 @@ package Linkedin;
 
 public class LongestPalindromicSubstring {
 
-    static boolean isPalindrome(String s, int low, int high) {
-        while (low < high) {
-            if (s.charAt(low) != s.charAt(high)) {
-                return false;
-            }
-            low++;
-            high--;
-        }
-        return true;
-    }
-
-    static String findLongestPalindromeSubstring(String s) {
-        int n = s.length();
-        int maxLength = 1; // Single-character palindromes are the smallest valid palindromes
-        int startIndex = 0;
-
-        for (int start = 0; start < n; start++) {
-            for (int end = start; end < n; end++) {
-                int currentLength = end - start + 1; // Length of the current substring
-                if (isPalindrome(s, start, end) && currentLength > maxLength) {
-                    maxLength = currentLength;
-                    startIndex = start;
-                }
-            }
-        }
-        return s.substring(startIndex, startIndex + maxLength);
-    }
-
     public static void main(String[] args) {
         String str = "babad";
         System.out.println("Longest Palindromic Substring: " + findLongestPalindromeSubstring(str));
         str = "cbbd";
         System.out.println("Longest Palindromic Substring: " + findLongestPalindromeSubstring(str));
     }
-}
 
+    static boolean isPalindrome(String str, int start, int end) {
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+
+    static String findLongestPalindromeSubstring(String s) {
+        int maxLength = 1;
+        String longestPalindrome = String.valueOf(s.charAt(0));
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if ((j - i) > maxLength && isPalindrome(s, i, j)) {
+                    maxLength = j - i;
+                    longestPalindrome = s.substring(i, j + 1);
+                }
+            }
+        }
+        return longestPalindrome;
+    }
+}
--- src/test/java/RandomPractice/LongestPalindromicSubstring.java
+++ src/test/java/RandomPractice/LongestPalindromicSubstring.java
@@ -7,6 +7,28 @@ public class LongestPalindromicSubstring {
     }
 
     static String findLongestPalindromeSubstring(String s) {
-        return null;
+        int maxLength = 1;
+        String longestPalindrome = String.valueOf(s.charAt(0));
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if ((j - i) > maxLength && isPalindrome(s, i, j)) {
+                    maxLength = j - i;
+                    longestPalindrome = s.substring(i, j + 1);
+                }
+            }
+        }
+        return longestPalindrome;
+    }
+
+    static boolean isPalindrome(String str, int start, int end) {
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+        return true;
     }
 }
