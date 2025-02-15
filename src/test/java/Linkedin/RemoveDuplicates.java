f57a781304e01e161b929e351e9a8ff957784e32
Saurav Singh <singh.saurav@icloud.com>
2025-02-15 09:33:46 +0530

    added Linkedin Code

--- src/test/java/Linkedin/RemoveDuplicates.java
+++ src/test/java/Linkedin/RemoveDuplicates.java
@@ -1,41 +1,26 @@
 package Linkedin;
 
 import org.testng.Assert;
+
 import java.util.Arrays;
 
 public class RemoveDuplicates {
-    //t:nlogn
-    //S:1
     public static void main(String[] args) {
-        int[] arr = {1, 1, 2, 3, 4, 4, 5};
-        Assert.assertEquals(removeDuplicates(arr),new int[]{2,3,1,4,5});
-        int[] arr1 = {1, 1, 2, 3, 4, 4, 5};
-        Assert.assertEquals(removeDuplicatesInsOrderMaintain(arr1),new int[]{1,2,3,4,5});
+        int[] arr = {1, 1, 2, 3, 4, 4, 4, 5};
+        Assert.assertEquals(removeDuplicates(arr), new int[]{1, 2, 3, 4, 5});
     }
 
     public static int[] removeDuplicates(int[] arr) {
+        if (arr == null || arr.length == 0) return new int[0];
         Arrays.sort(arr);
         int j = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1]) {
-                int temp = arr[i - 1];
-                arr[i - 1] = arr[j];
-                arr[j] = temp;
-                j++;
-            }
-        }
-        return Arrays.copyOfRange(arr, j, arr.length);
-    }
 
-    public static int[] removeDuplicatesInsOrderMaintain(int[] nums) {
-        if (nums.length == 0) return nums;
-        int j = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[j]) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {//Shifts unique elements forward instead of swapping
                 j++;
-                nums[j] = nums[i];
+                arr[j] = arr[i];
             }
         }
-        return Arrays.copyOfRange(nums, 0, j + 1);
+        return Arrays.copyOfRange(arr, 0, j + 1);
     }
 }
