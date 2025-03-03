3b31a356acda0c3886675f1a6704f852e682bf25
Saurav Singh <singh.saurav@icloud.com>
2025-03-03 08:36:40 +0530

    added Linkedin Code

--- src/test/java/Linkedin/LinkedListDeleteAllChar.java
+++ src/test/java/Linkedin/LinkedListDeleteAllChar.java
@@ -1,9 +1,9 @@
 package Linkedin;
 
 public class LinkedListDeleteAllChar {
-    //    Write a Program to create a linked list with following inputs and Delete all occurrences of a given key in a linked list.
-    //    * Input: head: 3 -> 3 -> 6 -> 9 -> 3 -> NULL, key = 3
-    //    * Output: 6 -> 9 -> NULL, i.e., we need to delete All occurrences of the given key = 3 from the Linked List
+    //Write a Program to create a linked list with following inputs and Delete all occurrences of a given key in a linked list.
+    //Input: head: 3 -> 3 -> 6 -> 9 -> 3 -> NULL, key = 3
+    //Output: 6 -> 9 -> NULL, i.e., we need to delete All occurrences of the given key = 3 from the Linked List
 
     public static void main(String[] args) {
         CustomLinkedList list = new CustomLinkedList(10);
@@ -15,5 +15,4 @@ public class LinkedListDeleteAllChar {
         list.deleteKey(10);
         list.printList();
     }
-
 }
