02b89cd39b8c47fec6ea29f3cb22f4d6e4fd0c6b
Saurav Singh <singh.saurav@icloud.com>
2025-03-04 08:27:03 +0530

    added Linkedin Code

--- src/test/java/Linkedin/NonRepChar.java
+++ src/test/java/Linkedin/NonRepChar.java
@@ -7,43 +7,35 @@ public class NonRepChar {
     // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1
 
     public static void main(String[] args) {
-        System.out.println(nonRepCharWithMap("geeksforgeeks"));  // Expected output: 5 (f)
-        //Explanation: ‘f’ is the first character in the string which does not repeat.
+        System.out.println(nonRepCharWithMap("geeksforgeeks"));
         System.out.println(nonRepChar("geeksforgeeks"));
     }
 
-//Time Complexity: O(N)
-//Space Complexity: O(N)
+    //Time Complexity: O(N)
+    //Space Complexity: O(N)
     public static int nonRepCharWithMap(String str) {
         Map<Character, Integer> hm = new HashMap<>();
         for (Character c : str.toLowerCase().toCharArray()) {
             hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
-
         for (int i = 0; i < str.length(); i++) {
-            if (hm.get(str.charAt(i)) == 1)
-                return i;
+            if (hm.get(str.charAt(i)) == 1) return i;
         }
         return -1;
     }
 
-//Time Complexity: O(N)
-//Space Complexity: O(1)
+    //Time Complexity: O(N)
+    //Space Complexity: O(1)
     public static int nonRepChar(String s) {
         int[] freq = new int[26];
-
-        // Count the frequency of each character
         for (char c : s.toCharArray()) {
             freq[c - 'a']++;
         }
-
-        // Find the first character with frequency 1
         for (int i = 0; i < s.length(); i++) {
             if (freq[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
-
         return -1;
     }
 }
