82ad619b7de8749d3e4efc0098c3d934bd0bc0ea
Saurav Singh <singh.saurav@icloud.com>
2025-03-04 08:56:26 +0530

    added Linkedin Code

--- src/test/java/Linkedin/SeparateEvenOdd.java
+++ src/test/java/Linkedin/SeparateEvenOdd.java
@@ -6,11 +6,11 @@ public class SeparateEvenOdd {
     //Shift all even numbers to left side of array and odd number to right side.
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        moveNegatives(arr);
+        moveEven(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void moveNegatives(int[] nums) {
+    private static void moveEven(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return;
         }
@@ -18,10 +18,8 @@ public class SeparateEvenOdd {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
                 int temp = nums[i];
-                for (int k = i; k > j; k--) {
-                    nums[k] = nums[k - 1];
-                }
-                nums[j] = temp;
+                nums[i]= nums[j];
+                nums[j]=temp;
                 j++;
             }
         }
