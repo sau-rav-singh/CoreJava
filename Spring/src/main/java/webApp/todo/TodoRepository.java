119de312000d7e568d83a5f10cad1b19a6bc8614
Saurav Singh <singh.saurav@icloud.com>
2025-04-13 18:12:06 +0530

    added rest

--- Spring/src/main/java/webApp/todo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package webApp.todo;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer>{
-    public List<Todo> findByUsername(String username);
-}
