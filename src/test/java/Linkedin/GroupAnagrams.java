442364172dd5cef95c2beb702c0879c56e459bf4
Saurav Singh <singh.saurav@icloud.com>
2026-07-23 21:36:29 +0530

    added question in comments

--- src/test/java/Linkedin/GroupAnagrams.java
+++ src/test/java/Linkedin/GroupAnagrams.java
@@ -1,8 +1,6 @@
 package Linkedin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 // QUESTION: Given an array of strings, group the anagrams together. An anagram is a word or phrase
 // formed by rearranging the letters of a different word or phrase.
@@ -17,58 +15,14 @@ public class GroupAnagrams {
         }
     }
 
-    public static List<List<String>> groupAnagrams1(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        String[] sortedStrings = new String[strs.length];
-
-        for (int i = 0; i < strs.length; i++) {
-            char[] arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            sortedStrings[i] = new String(arr);
-        }
-
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i] == null) continue;
-            List<String> group = new ArrayList<>();
-            group.add(strs[i]);
-
-            for (int j = i + 1; j < strs.length; j++) {
-                if (strs[j] != null && sortedStrings[i].equals(sortedStrings[j])) {
-                    group.add(strs[j]);
-                    strs[j] = null;
-                }
-            }
-            result.add(group);
-            strs[i] = null;
-        }
-        return result;
-    }
-
     public static List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        boolean[] isGrouped = new boolean[strs.length];
-        for (int i = 0; i < strs.length; i++) {
-            List<String> subList = new ArrayList<>();
-            if (!isGrouped[i]) {
-                subList.add(strs[i]);
-                for (int j = i + 1; j < strs.length; j++) {
-                    if (areAnagrams(strs[i], strs[j])) {
-                        subList.add(strs[j]);
-                        isGrouped[j] = true;
-                    }
-                }
-            }
-            if (!subList.isEmpty())
-                result.add(subList);
+        Map<String, List<String>> anagramMap = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            anagramMap.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
         }
-        return result;
-    }
-
-    static boolean areAnagrams(String s1, String s2) {
-        char[] a1 = s1.toCharArray();
-        char[] a2 = s2.toCharArray();
-        Arrays.sort(a1);
-        Arrays.sort(a2);
-        return Arrays.equals(a1, a2);
+        return new ArrayList<>(anagramMap.values());
     }
 }
